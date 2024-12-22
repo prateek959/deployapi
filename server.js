503a5fc91c9d1f1ddc8649c72b4e84f6ab0dadc0
Deepeshnama <deepeshnama0786@gmail.com>
2024-12-23 03:05:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "*",
+    origin: 'http://localhost:5173',
     credentials: true,
   },
 });
